08c4edd454c8162ffd5bd9a80d8b57c6e9791bce
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2024-03-15 13:56:54 +0000

    question/answer listing style changes 2

--- src/components/QuestionListing.jsx
+++ src/components/QuestionListing.jsx
@@ -1,7 +1,7 @@
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 function QuestionListing({ question, id, modalOpen, }) {
     return (
-        <a onClick={() => { modalOpen(id) }} className="flex flex-row w-9/12 justify-between cursor-pointer border-b-4 border p-3 border-opacity-100">
+        <a onClick={() => { modalOpen(id) }} className="flex flex-row w-9/12 justify-between cursor-pointer border-b-4 border p-3 border-opacity-100 rounded-xl">
             {/* h1 text should wrap behind 60%, after 2 lines - chars h1 gets ... and no more is added */}
             <h1 className="text-2xl font-sans w-2/3 my-3 py-2">{question}</h1>
             <div>
