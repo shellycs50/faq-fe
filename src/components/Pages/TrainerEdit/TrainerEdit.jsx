916212e50b3a7854eeec4973326a516319bdeb84
Rob Sheldrick <lebaguettemuncherer@gmail.com>
2024-03-18 18:07:19 +0000

    formatted previous changes

--- src/components/Pages/TrainerEdit/TrainerEdit.jsx
+++ src/components/Pages/TrainerEdit/TrainerEdit.jsx
@@ -11,14 +11,14 @@ function TrainerEdit() {
         selectedLangId: null,
     });
     const [selectedLangId, setSelectedLangId] = useState(undefined);
-    const [title, setTitle] = useState(""); 
+    const [title, setTitle] = useState("");
     const [uploadError, setUploadError] = useState("");
     const editorRef = useRef(null);
     const navigate = useNavigate();
     const { id } = useParams();
     let nonStateTitle = ""
     let nonStateLang = null
-   
+
 
     async function fetchQuestion(id) {
         const response = await fetch(`https://faq-api-demo.robsheldrick.dev.io-academy.uk/api/trainer/faq/${id}`, {
@@ -45,7 +45,7 @@ function TrainerEdit() {
                 title: question.question,
                 selectedLangId: question.language_id,
             });
-            
+
         };
 
         fetchData();
@@ -68,73 +68,73 @@ function TrainerEdit() {
         if (title !== cache.title) {
             bodyObj['question_rename'] = title;
         }
-        
+
         try {
-          const response = await fetch("https://faq-api-demo.robsheldrick.dev.io-academy.uk/api/trainer/faq", {
-            method: "PUT",
-            headers: {
-                'Authorization': `Bearer ${Cookies.get('auth_key')}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(bodyObj),
-          });
-      
-          const result = await response.json();
-          if (result.message === "Success") {
-            navigate("/success");
-        }
-        if (result.message === "Validation error") {
-            let errorstr = "";
-            for (const key in result.errors) {
-                errorstr += result.errors[key];
+            const response = await fetch("https://faq-api-demo.robsheldrick.dev.io-academy.uk/api/trainer/faq", {
+                method: "PUT",
+                headers: {
+                    'Authorization': `Bearer ${Cookies.get('auth_key')}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(bodyObj),
+            });
+
+            const result = await response.json();
+            if (result.message === "Success") {
+                navigate("/success");
             }
-            setUploadError(errorstr);   
-        }
-            
+            if (result.message === "Validation error") {
+                let errorstr = "";
+                for (const key in result.errors) {
+                    errorstr += result.errors[key];
+                }
+                setUploadError(errorstr);
+            }
+
         } catch (error) {
             setUploadError(error)
         }
-      }
+    }
 
-      function resetToCache() {
+    function resetToCache() {
         setSelectedLangId(cache.selectedLangId);
         setTitle(cache.title);
-      }
+    }
 
     return (
         <div className="bg-slate-500 h-screenminusnav">
-    <div className="rounded-lg bg-slate-800 pb-10">
-        <h1 className="text-4xl text-center font-bold p-10 bg-slate-500 text-white rounded-lg">Quick Add</h1>
-        <form className="mt-10">
-            <div className="flex flex-row justify-around h-full w-full">
-                <div className="px-10 flex flex-col justify-start gap-12">
-                    <div className="flex flex-col gap-1">
-                        <label htmlFor='langselect' className="text-sm text-gray-500">Language</label>
-                        <LanguageSelect id='langselect' selectstyles="text-lg rounded-lg p-1" optionstyles="text-lg" selectedLangId={selectedLangId} setSelectedLangId={setSelectedLangId} />
+            <div className="rounded-lg bg-slate-800 pb-10">
+                <h1 className="text-4xl text-center font-bold p-10 bg-slate-500 text-white rounded-lg">Quick Add</h1>
+                <form className="mt-10">
+                    <div className="flex flex-row justify-around h-full w-full">
+                        <div className="px-10 flex flex-col justify-start gap-12">
+                            <div className="flex flex-col gap-1">
+                                <label htmlFor='langselect' className="text-sm text-gray-500">Language</label>
+                                <LanguageSelect id='langselect' selectstyles="text-lg rounded-lg p-1" optionstyles="text-lg" selectedLangId={selectedLangId} setSelectedLangId={setSelectedLangId} />
+                            </div>
+
+                            <div className="mb-2 flex flex-col gap-1">
+                                <label className="text-sm text-gray-500">Question/Title</label>
+                                <textarea
+                                    id="title"
+                                    name="title"
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    className="border-4 mt-1 p-2 w-full rounded-md text-lg resize-none"
+                                    style={{ height: "200px" }}
+                                />
+                            </div>
+                            {uploadError != "" && <h4 className="pb-6 text-lg text-red-500">{uploadError}</h4>}
+                        </div>
+                        <QuestionBuilder editorRef={editorRef} />
                     </div>
-                    
-                    <div className="mb-2 flex flex-col gap-1">
-                        <label className="text-sm text-gray-500">Question/Title</label>
-                        <textarea
-                            id="title"
-                            name="title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            className="border-4 mt-1 p-2 w-full rounded-md text-lg resize-none"
-                            style={{ height: "200px" }}
-                        />
+                    <div className="flex flex-row justify-center gap-5">
+                        <a onClick={resetToCache} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition cursor-pointer">Reset All Values</a>
+                        <button type='submit' onClick={submitHandler} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition cursor-pointer">Submit</button>
                     </div>
-                    {uploadError != "" && <h4 className="pb-6 text-lg text-red-500">{uploadError}</h4>}
-                </div>
-                <QuestionBuilder editorRef={editorRef}/>
+                </form>
             </div>
-            <div className="flex flex-row justify-center gap-5">
-                <a onClick={resetToCache} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition cursor-pointer">Reset All Values</a> 
-                <button type='submit' onClick={submitHandler} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition cursor-pointer">Submit</button>
-            </div>
-        </form>
-    </div>
-</div>
+        </div>
 
     );
 }
--- src/components/Pages/TrainerPost/TrainerPost.jsx
+++ src/components/Pages/TrainerPost/TrainerPost.jsx
@@ -6,30 +6,30 @@ import QuestionBuilder from "../../QuestionBuilder";
 
 function TrainerPost() {
     const [selectedLangId, setSelectedLangId] = useState(0);
-    const [title, setTitle] = useState(""); 
+    const [title, setTitle] = useState("");
     const [uploadError, setUploadError] = useState("");
     const editorRef = useRef(null);
     const navigate = useNavigate();
-  
+
     async function submitHandler(event) {
         event.preventDefault();
         setUploadError(false);
         const answer = editorRef.current.getContent();
         try {
-          const response = await fetch("https://faq-api-demo.robsheldrick.dev.io-academy.uk/api/trainer/faq", {
-            method: "POST",
-            headers: {
-                'Authorization': `Bearer ${Cookies.get('auth_key')}`,
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-                question: title,
-                answer: answer,
-                language_id: selectedLangId,
-            }),
-          });
-          
-          const result = await response.json();
+            const response = await fetch("https://faq-api-demo.robsheldrick.dev.io-academy.uk/api/trainer/faq", {
+                method: "POST",
+                headers: {
+                    'Authorization': `Bearer ${Cookies.get('auth_key')}`,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    question: title,
+                    answer: answer,
+                    language_id: selectedLangId,
+                }),
+            });
+
+            const result = await response.json();
             if (result.message === "Success") {
                 navigate("/success");
             }
@@ -38,45 +38,45 @@ function TrainerPost() {
                 for (const key in result.errors) {
                     errorstr += result.errors[key];
                 }
-                setUploadError(errorstr);   
+                setUploadError(errorstr);
             }
         } catch (error) {
             setUploadError(error)
         }
-      }
+    }
 
     return (
         <div className="bg-slate-500 h-screenminusnav">
-        <div className="rounded-lg bg-slate-800 pb-10">
-            <h1 className="text-4xl text-center font-bold p-10 bg-slate-500 text-white rounded-lg">Add a Question and Answer in one go.</h1>
-            <form className="mt-10">
-                <div className="flex flex-row justify-around h-full w-full">
-                <div className="px-10 flex flex-col justify-start gap-12"> 
-                <div className="flex flex-col gap-1">
-                    <label htmlFor='langselect' className="text-sm text-gray-500">Language</label>
-                    <LanguageSelect id='langselect' selectstyles="text-lg rounded-lg p-1" optionstyles="text-lg" selectedLangId={selectedLangId} setSelectedLangId={setSelectedLangId} />
+            <div className="rounded-lg bg-slate-800 pb-10">
+                <h1 className="text-4xl text-center font-bold p-10 bg-slate-500 text-white rounded-lg">Add a Question and Answer in one go.</h1>
+                <form className="mt-10">
+                    <div className="flex flex-row justify-around h-full w-full">
+                        <div className="px-10 flex flex-col justify-start gap-12">
+                            <div className="flex flex-col gap-1">
+                                <label htmlFor='langselect' className="text-sm text-gray-500">Language</label>
+                                <LanguageSelect id='langselect' selectstyles="text-lg rounded-lg p-1" optionstyles="text-lg" selectedLangId={selectedLangId} setSelectedLangId={setSelectedLangId} />
+                            </div>
+
+                            <div className="mb-2 flex flex-col gap-1">
+                                <label className="text-sm text-gray-500">Question/Title</label>
+                                <textarea
+                                    id="title"
+                                    name="title"
+                                    value={title}
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    className="border-4 mt-1 p-2 w-full rounded-md text-lg resize-none"
+                                    style={{ height: "200px" }}
+                                />
+                            </div>
+                            <h4 className="pb-6 text-lg text-red-500">{uploadError != "" && uploadError}</h4>
+                        </div>
+                        <QuestionBuilder editorRef={editorRef} />
                     </div>
-                    
-                    <div className="mb-2 flex flex-col gap-1">
-                        <label className="text-sm text-gray-500">Question/Title</label>
-                        <textarea
-                            id="title"
-                            name="title"
-                            value={title}
-                            onChange={(e) => setTitle(e.target.value)}
-                            className="border-4 mt-1 p-2 w-full rounded-md text-lg resize-none"
-                            style={{ height: "200px" }}
-                        />
+                    <div className="flex flex-row justify-center gap-5">
+                        <button type='submit' onClick={submitHandler} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition cursor-pointer">Submit</button>
                     </div>
-                    <h4 className="pb-6 text-lg text-red-500">{uploadError != "" && uploadError}</h4>
-                </div>
-                <QuestionBuilder editorRef={editorRef}/>
-                </div>
-                <div className="flex flex-row justify-center gap-5"> 
-                <button type='submit' onClick={submitHandler} className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 transition cursor-pointer">Submit</button>
-                </div>
-            </form>
-        </div>
+                </form>
+            </div>
         </div>
     );
 }
